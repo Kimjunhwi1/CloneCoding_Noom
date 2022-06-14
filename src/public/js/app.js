26c57959dad2d16e1bbff25f245284f860c26cb4
Kimjunhwi <ppo976@naver.com>
2022-06-14 18:40:36 +0900

    Noom_ws_ver0.1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,3 @@
 // alert("hi!");
 //const socket = new WebSocket("http://localhost:3000")
-const socket = new WebSocket(`ws://$(window.location.host)`)
+const socket = new WebSocket(`ws://${window.location.host}`);
