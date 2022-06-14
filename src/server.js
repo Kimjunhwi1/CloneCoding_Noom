daf96e13d1694ce22d6c60fd3e6ec988bf1ed7b4
Kimjunhwi <ppo976@naver.com>
2022-06-14 18:15:51 +0900

    noomws_v0.1

--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
 // console.log('hello');
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-// _dirname -> Node.js 기본 전역 변수, 현재 실행하는 폴더의 경로
-// 이 프로젝트에서는 Noom의 src 폴더 경로가 할당되어 있음.
 app.use("/public", express.static(__dirname + "/public"))
 app.get("/",  (req, res)  => res.render("home"));
 app.get("/*",  (req, res)  => res.render("/"));
-// 어떤 주소를 입력해ㅓㅅ 서버에 접근하더라도 사용자는 home.pug를 받음
-const handleListen = () => console.log("Listenling on http://localhost:3000")
-app.listen(3000,handleListen);
+
+const handleListen = () => console.log("Listenling on http://localhost:3000");
+//app.listen(3000,handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket){
+    console.log(socket)
+};
+
+wss.on("connection",handleConnection);
+server.listen(3000, handleListen);
