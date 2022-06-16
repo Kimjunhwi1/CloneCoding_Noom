b5934e7fa1abdb05fad2b23c640c60f4586f81a5
Kimjunhwi <ppo976@naver.com>
2022-06-16 18:08:34 +0900

    noom_ws_event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,19 @@
 // alert("hi!");
 //const socket = new WebSocket("http://localhost:3000")
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open",() => {
+    console.log("Connected to Server")
+});
+
+socket.addEventListener("message",(message) => {
+    console.log("just got this", message.data, "from the server")
+});
+
+socket.addEventListener("close",() => {
+    console.log("diseConnected to Server")
+});
+
+setTimeout(() => {
+    socket.send("hello from browsers")
+},5000);
